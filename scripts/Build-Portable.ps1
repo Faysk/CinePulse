8b78884a68ca3e5d6d33c3ea766d1e5574e52ec0
Faysk <faysk.nan@gmail.com>
2026-09-21 01:36:20 +0100

    CI: keep audio timeline proof on Stable 1.2.9 (scripts/Build-Portable.ps1)

--- scripts/Build-Portable.ps1
+++ scripts/Build-Portable.ps1
@@ -1,6 +1,6 @@
 ﻿[CmdletBinding()]
 param(
-    [string]$Version = '1.2.10',
+    [string]$Version = '1.2.9',
     [string]$Repository = '',
     [string]$MinisignPublicKey = '',
     [string]$MinisignSecretKey = '',
