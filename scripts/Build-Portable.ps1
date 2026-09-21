bfa0d31917b8de004fcfd9d4a26bc10f61614226
Faysk <faysk.nan@gmail.com>
2026-09-21 01:39:39 +0100

    release: bump CinePulse to 1.2.10 (scripts/Build-Portable.ps1)

--- scripts/Build-Portable.ps1
+++ scripts/Build-Portable.ps1
@@ -1,6 +1,6 @@
 ﻿[CmdletBinding()]
 param(
-    [string]$Version = '1.2.9',
+    [string]$Version = '1.2.10',
     [string]$Repository = '',
     [string]$MinisignPublicKey = '',
     [string]$MinisignSecretKey = '',
